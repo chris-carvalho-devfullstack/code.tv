54a3c2cdaff443f1d6b2430573d77a5964fb6100
christian <christian.carvalho.adv@gmail.com>
2026-04-17 15:29:38 -0300

    fix: ignorando explicit-any com eslint disable

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -14,7 +14,8 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        // AQUI FOI FEITA A CORREÇÃO DA TIPAGEM DO TYPESCRIPT
+        // O comentário abaixo desativa o erro do ESLint para a próxima linha
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({ request })
