ef1a0475989ef583145a7dd5f1fde7a638782468
christian <christian.carvalho.adv@gmail.com>
2026-04-25 21:16:36 -0300

    criacão de layout.tsx na pasta minha conta para rodar no edge da cloudfare

---
+++ src/app/minha-conta/layout.tsx
@@ -0,0 +1,10 @@
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+export default function MinhaContaLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
